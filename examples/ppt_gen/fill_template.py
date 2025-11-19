29e3f30c43367af979e9460196d6650f64fa2b94
fpg2012 <fpg2012@foxmail.com>
2025-11-19 16:37:03 +0800

    remove redundant code

--- examples/ppt_gen/fill_template.py
+++ examples/ppt_gen/fill_template.py
@@ -7,29 +7,6 @@ from ppt_template_model import PageConfig
 from pptx import Presentation
 from utils import delete_slide, delete_slide_range, duplicate_slide, move_slide
 
-# def fill_template(template_path, output_path, json_data):
-#     """
-#     Fill the template with the given json data.
-#     """
-#     prs = Presentation(template_path)
-#     data = json.loads(json_data)
-#     slides = parse_json(data)
-#     for slide in slides:
-#         if slide.type == "title":
-#             title_slide = prs.slides[TYPE_MAP[slide.type]]
-#             slide.render(title_slide)
-#         elif slide.type == "acknowledgement":
-#             acknowledgement_slide = prs.slides[TYPE_MAP[slide.type]]
-#             slide.render(acknowledgement_slide)
-#         else:
-#             new_slide = duplicate_slide(prs, prs.slides[TYPE_MAP[slide.type]])
-#             slide.render(new_slide)
-
-#     delete_slide_range(prs, range(2, 12))
-#     delete_slide(prs, 0)
-#     move_slide(prs, 1, len(prs.slides) - 1)
-#     prs.save(output_path)
-
 
 def fill_template_with_yaml_config(template_path, output_path, json_data, yaml_config: dict[str, Any]):
     page_config = PageConfig(yaml_config)
